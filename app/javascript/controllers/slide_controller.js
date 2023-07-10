4e61fb965f020bad6f29f72268a0c77535ca642b
abchung <anthony.b.chung@gmail.com>
2023-07-10 11:02:26 +1000

    stimulus to move project window

--- app/javascript/controllers/slide_controller.js
+++ app/javascript/controllers/slide_controller.js
@@ -2,6 +2,36 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="slide"
 export default class extends Controller {
-  connect() {
+
+  static targets = ["slidingWindow"]
+  static values = {offSet: Number, projects: Number, projectWindow: Number}
+
+  initialize() {
+    this.offSetValue = 0
+    this.slidingWindowTarget.style.left = `${this.offSetValue}px`
+    this.projectWindowValue = (this.projectsValue-4) * 160 *  -1
+    console.log(this.projectWindowValue)
+  }
+
+  left(){
+    if(this.offSetValue < 0){
+      this.offSetValue += 160
+    }
+
+    if(this.offSetValue > 0){
+      this.offSetValue = 0
+    }
+    this.slidingWindowTarget.style.left = `${this.offSetValue}px`
+  }
+
+  right(){
+    if(this.offSetValue > this.projectWindowValue){
+      this.offSetValue -= 160
+    } else {
+      this.offSetValue = this.projectWindowValue
+    }
+
+    this.slidingWindowTarget.style.left = `${this.offSetValue}px`
+    console.log(this.offSetValue)
   }
 }
